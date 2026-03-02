49acb708532ac9d2b278d7910e665c4dfc07d69e
Hugnd-UIT <hungnd.attt2024@gmail.com>
2026-03-02 23:36:12 +0700

    Code middleware de xac thuc nguoi dung phia backend

--- services/routes/api.php
+++ services/routes/api.php
@@ -7,27 +7,26 @@ use App\Http\Controllers\PayrollApiController;
 use App\Http\Controllers\RosterApiController;
 use App\Http\Controllers\ProfileApiController;
 use App\Http\Controllers\SalaryApiController;
+use App\Http\Middleware\CheckKeycloakToken;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/payrolls', [PayrollApiController::class, 'index']);
+Route::middleware([CheckKeycloakToken::class])->group(function() {
+    Route::get('/payrolls', [PayrollApiController::class, 'index']);
 
-Route::get('/employees', [RosterApiController::class, 'index']);
+    Route::get('/employees', [RosterApiController::class, 'index']);
 
-Route::post('/employees', [RosterApiController::class, 'store']);
+    Route::post('/employees', [RosterApiController::class, 'store']);
+    Route::get('/employees/{id}', [RosterApiController::class, 'show']);
+    Route::put('/employees/{id}', [RosterApiController::class, 'update']);
+    Route::delete('/employees/{id}', [RosterApiController::class, 'destroy']);
 
-Route::get('/employees/{id}', [RosterApiController::class, 'show']);
+    Route::get('/profile/{manv}', [ProfileApiController::class, 'showProfile']);
+    Route::get('/salary/{manv}', [SalaryApiController::class, 'showSalary']);
 
-Route::put('/employees/{id}', [RosterApiController::class, 'update']);
+    Route::get('dashboard/manager-data', [DashboardApiController::class, 'get_manager_data']);
 
-Route::delete('/employees/{id}', [RosterApiController::class, 'destroy']);
-
-Route::get('/profile/{manv}', [ProfileApiController::class, 'showProfile']);
-
-Route::get('dashboard/manager-data', [DashboardApiController::class, 'get_manager_data']);
-
-Route::get('dashboard/employee-data', [DashboardApiController::class, 'get_employee_data']);
-
-Route::get('/salary/{manv}', [SalaryApiController::class, 'showSalary']);
+    Route::get('dashboard/employee-data', [DashboardApiController::class, 'get_employee_data']);
+});
