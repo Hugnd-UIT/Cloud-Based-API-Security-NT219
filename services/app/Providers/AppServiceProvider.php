bf9fbd7fb6d93a99cd67b0fc1da7a35c60a95559
Hugnd-UIT <hungnd.attt2024@gmail.com>
2026-04-03 16:56:29 +0700

    Fix bug vault

--- services/app/Providers/AppServiceProvider.php
+++ services/app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $vaultUrl = env('VAULT_URL', 'http://payshield_vault:8200') . '/v1/secret/data/payshield';
+        $token = env('VAULT_APP_TOKEN');
+
+        try {
+            /** @var \Illuminate\Http\Client\Response $response */
+
+            $response = Http::withHeaders([
+                'X-Vault-Token' => $token
+            ])->timeout(3)->get($vaultUrl);
+
+            if ($response->successful()) {
+                $secrets = $response->json()['data']['data'];
+                Config::set('database.connections.mysql.username', $secrets['DB_USERNAME']);
+                Config::set('database.connections.mysql.password', $secrets['DB_PASSWORD']);
+                Config::set('database.connections.mysql.database', $secrets['DB_DATABASE']);
+                Config::set('app.key', $secrets['APP_KEY']);
+                Config::set('services.keycloak.admin_user', $secrets['KC_ADMIN_USER'] ?? null);
+                Config::set('services.keycloak.admin_password', $secrets['KC_ADMIN_PASSWORD'] ?? null);
+                Config::set('services.payshield.client_secret', $secrets['CLIENT_SECRET'] ?? null);
+                
+                DB::purge('mysql');
+            }
+        } catch (\Exception $e) {
+            Log::error('Không thể kết nối tới Vault: ' . $e->getMessage());
+        }
     }
 }
---
+++ services/payshield-policy.hcl
@@ -0,0 +1,3 @@
+path "secret/data/payshield" {
+  capabilities = ["read"]
+}
