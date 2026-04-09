19b77211e81c72007925e1d0310e1593b061ac5a
Hugnd-UIT <hungnd.attt2024@gmail.com>
2026-04-09 09:07:23 +0700

    Fix bug keycloak

--- kms/vault-unseal.ps1
+++ kms/vault-unseal.ps1
@@ -2,11 +2,11 @@ Write-Host ">>> Reseting... <<<" -ForegroundColor Yellow
 
 docker exec -it payshield_vault vault operator unseal -ca-cert=/vault/certs/ca.crt -reset
 
-docker exec -it payshield_vault sh -c "vault operator unseal -ca-cert=/vault/certs/ca.crt '6VAzjVmCfZSUoLiPF4u6d8EG8S/p6z3U6RIY+8uFUknI'"
-docker exec -it payshield_vault sh -c "vault operator unseal -ca-cert=/vault/certs/ca.crt 'cDEQYawiYYD/oqShIOJAtSemgarCKMEgmAjs+Zfh3WPt'"
-docker exec -it payshield_vault sh -c "vault operator unseal -ca-cert=/vault/certs/ca.crt '2EyotQ9LN2QorbSQFC65jH59ThL94m8uIW1HxIClO18h'"
-docker exec -it payshield_vault sh -c "vault operator unseal -ca-cert=/vault/certs/ca.crt '8W07WWudkXahJA59J1VLmM8xZIAubcywfYIMqaWIHvyk'"
-docker exec -it payshield_vault sh -c "vault operator unseal -ca-cert=/vault/certs/ca.crt 'aL486Bfn3D3h2LyJFiDx/jf/7+IktQ/orlghVogGc808'"
+docker exec -it payshield_vault sh -c "vault operator unseal -ca-cert=/vault/certs/ca.crt 'lzKSZCVAcUJPetWgsAuCOI3GBQj7CAhxNk/PsAmHcHii'"
+docker exec -it payshield_vault sh -c "vault operator unseal -ca-cert=/vault/certs/ca.crt 'F9gZ4tHXpOinWl0TZkm6gNntjlyvibYJqfyZQuxk0YYs'"
+docker exec -it payshield_vault sh -c "vault operator unseal -ca-cert=/vault/certs/ca.crt 'rzIHBFwOBmuEtJgCO4qYL1VegNn5NOyfrLsL5PsnC3xU'"
+docker exec -it payshield_vault sh -c "vault operator unseal -ca-cert=/vault/certs/ca.crt '1fPY1hB1/SA2En0I48uZ4yagHWcf9yQzBr7OpM1bhcaH'"
+docker exec -it payshield_vault sh -c "vault operator unseal -ca-cert=/vault/certs/ca.crt 'Dhc2mGDQ2jMVTX48F1f/SXsOyLjiQwh83DE+urkOL7B+'"
 docker exec -it payshield_vault vault status -ca-cert=/vault/certs/ca.crt
 
 Write-Host ">>> Vault Unsealed Successfully! <<<" -ForegroundColor Green
